c1bc5b77bcb30eb67999cc3023073cfdd609f955
reyt1 <reynaldo.ns@gmail.com>
2016-12-15 07:15:37 -0300

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,1 +1,18 @@
+#3rd question, plot "plot3.png", width=number.add.width, height=number.add.height
+#Group total NEI emissions per year:
+
+library(ggplot2)
+library(dplyr)
+BaltEmissionsbyYear<-summarise(group_by(filter(NEI, fips == "24510"), year,type), Emissions=sum(Emissions))
+ggplot(BaltEmissionsbyYear, aes(x=factor(year),
+							                  y=Emissions, 
+                                fill=type,
+                                label = round(Emissions,2))) +
+                                geom_bar(stat="identity") +
+                                facet_grid(. ~ type) +
+                                xlab("year") +
+                                ylab(expression("total PM"[2.5]*" emission in tons")) +
+                                ggtitle(expression("PM"[2.5]*paste(" emissions in Baltimore ",
+                                       "City by various source types", sep="")))+
+                                geom_label(aes(fill = type), colour = "white", fontface = "bold")
 
