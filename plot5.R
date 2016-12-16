3a57a1fbc70b4f6d32c26f89961979185ca62a3b
reyt1 <reynaldo.ns@gmail.com>
2016-12-15 23:59:36 -0300

    Create plot5.R

---
+++ plot5.R
@@ -0,0 +1,14 @@
+5th question, emissions from motor vehicle-Baltimore city
+library(ggplot2)
+library(dplyr)
+BaltimoreEmissions<-NEI[(NEI$fips=="24510") & (NEI$type=="ON-ROAD"),]
+BalbyYear <- summarise(group_by(BaltimoreEmissions, year), Emissions=sum(Emissions))
+ggplot(BalbyYear, aes(x=factor(year), 
+				y=Emissions,
+				fill=year, 
+				label = round(Emissions,2))) +
+    				geom_bar(stat="identity", colour="black") +
+    				xlab("year") +
+    				ylab(expression("total PM"[2.5]*" emissions in tons")) +
+    				ggtitle("Emissions from motor vehicle sources in Baltimore City")+
+    				geom_label(aes(fill = year),colour = "white", fontface = "bold")
